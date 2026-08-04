3b08d8fa59b502f380ab10d5345e70057943d8f5
drzzln <drzzln@protonmail.com>
2026-08-03 19:11:01 -0700

    fmt: canonicalize example .lisp corpus to feira fmt (pre-gate)

--- examples/pangea-tatara-akeyless/spec/arch-check.lisp
+++ examples/pangea-tatara-akeyless/spec/arch-check.lisp
@@ -3,11 +3,8 @@
 ;; unique-resource-names, unresolved refs, and public-ingress + tag
 ;; coverage; this file declares the caixa-specific additions.
 
-(defcheck vpc-has-owner-tag
-  (every-defteia :tipo aws/vpc
-    (has-kwarg :tags
-      (has-kwarg :owner))))
+(defcheck vpc-has-owner-tag (every-defteia :tipo aws/vpc (has-kwarg :tags (has-kwarg :owner))))
 
-(defcheck every-akeyless-target-has-region
-  (every-defteia :tipo akeyless/target-aws
-    (has-kwarg :region)))
+(defcheck
+  every-akeyless-target-has-region
+  (every-defteia :tipo akeyless/target-aws (has-kwarg :region)))
